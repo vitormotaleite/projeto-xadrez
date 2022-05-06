1eabca72d2185094f995228d3866d68854a9898a
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-05-06 13:24:59 -0300

    cuidando de pecas capturadas

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,6 +1,8 @@
 package program;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -15,11 +17,12 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 		
 		while(true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -33,6 +36,10 @@ public class Main {
 				ChessPosition target = UI.readChessPosition(sc);
 			
 				ChessPiece capturedPiece = chessMatch.peformChessMove(source, target);
+				
+				if(capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());

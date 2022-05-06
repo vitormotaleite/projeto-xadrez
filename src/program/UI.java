e2040695978d2b47f122e64359eae1bd49e0ac29
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-05-06 13:59:56 -0300

    logica de xeque

--- src/program/UI.java
+++ src/program/UI.java
@@ -59,6 +59,9 @@ public class UI {
 		System.out.println();
 		System.out.println("turn: " + chessMatch.getTurn());
 		System.out.println("waiting player: " + chessMatch.getCurrentPlayer());
+		if(chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {

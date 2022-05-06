db6d04aa50f4a26a1a308c0f0520853890a99e2e
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-05-06 09:42:47 -0300

    imprimindo movimentos possiveis

--- src/program/Main.java
+++ src/program/Main.java
@@ -23,6 +23,10 @@ public class Main {
 				System.out.println();
 				System.out.print("source: ");
 				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 			
 				System.out.println();
 				System.out.print("target: ");

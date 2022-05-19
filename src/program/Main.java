58cc043c8317c743838449297a169b4e3097101b
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-05-19 11:00:51 -0300

    promocao

--- src/program/Main.java
+++ src/program/Main.java
@@ -40,6 +40,12 @@ public class Main {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("enter piece for promotin (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
